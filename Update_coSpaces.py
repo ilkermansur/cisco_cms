3eb20c3dbd43d15a44c697e301f6e539155655e7
ilkermansur <59640302+ilkermansur@users.noreply.github.com>
2024-08-01 23:53:04 +0300

    Update Update_coSpaces.py
    beautiful code edit

--- Update_coSpaces.py
+++ Update_coSpaces.py
@@ -41,52 +41,58 @@ def add_tenant_to_cospace ():
     ID based on the prefix of the call ID.
     """
 
-    for user in cospace_list_parameters:
+    try:
+        for user in cospace_list_parameters:
 
-        call_id = user['callId']
+            call_id = user['callId']
 
-        if call_id.startswith('25'):
-            cospace_id = user['@id']
-            tenant_id = '7475dd16-1029-4bde-94ed-f249683146d8'
-            mod_url = url + cospace_id
-            payload = f"tenant={tenant_id}"
-            response = requests.put(mod_url,
-                                    auth= HTTPBasicAuth(username, password),
-                                    headers=headers,
-                                    data=payload,
-                                    verify=False
-                                    )
-            if response.status_code == 200:
-                user_space = user['name']
+            if call_id.startswith('25'):
+                cospace_id = user['@id']
+                tenant_id = '7475dd16-1029-4bde-94ed-f249683146d8'
+                mod_url = url + cospace_id
+                payload = f"tenant={tenant_id}"
+                response = requests.put(mod_url,
+                                        auth= HTTPBasicAuth(username, password),
+                                        headers=headers,
+                                        data=payload,
+                                        verify=False
+                                        )
+                if response.status_code == 200:
+                    user_space = user['name']
 
-                with open ("logs.txt", "a") as f:
-                    f.write (f'{user_space} was uptaded with new {tenant_id}\n')
-               
+                    with open ("logs.txt", "a") as f:
+                        f.write (f'{user_space} was uptaded with new {tenant_id}\n')
+                
+                else:
+                    with open ("logs.txt", "a") as f:
+                        f.write (f'error is occured {response.status_code}\n')
+
+            elif call_id.startswith('35'):
+                cospace_id = user['@id']
+                tenant_id = '71fc7729-e249-427d-9350-38da5f0e2a11'
+                mod_url = url + cospace_id
+                payload = f"tenant={tenant_id}"
+                response = requests.put(mod_url,
+                                        auth= HTTPBasicAuth(username, password),
+                                        headers=headers,
+                                        data=payload,
+                                        verify=False
+                                        )
+                if response.status_code == 200:
+                    with open ("logs.txt", "a") as f:
+                        f.write (f'{user_space} was uptaded with new {tenant_id}\n')
+                
+                else:
+                    with open ("logs.txt", "a") as f:
+                        f.write (f'error is occured {response.status_code}\n')
             else:
                 with open ("logs.txt", "a") as f:
-                    f.write (f'error is occured {response.status_code}\n')
+                    f.write (f'No need to UPDATE\n')
 
-        elif call_id.startswith('35'):
-            cospace_id = user['@id']
-            tenant_id = '71fc7729-e249-427d-9350-38da5f0e2a11'
-            mod_url = url + cospace_id
-            payload = f"tenant={tenant_id}"
-            response = requests.put(mod_url,
-                                    auth= HTTPBasicAuth(username, password),
-                                    headers=headers,
-                                    data=payload,
-                                    verify=False
-                                    )
-            if response.status_code == 200:
-                with open ("logs.txt", "a") as f:
-                    f.write (f'{user_space} was uptaded with new {tenant_id}\n')
-               
-            else:
-                with open ("logs.txt", "a") as f:
-                    f.write (f'error is occured {response.status_code}\n')
+    except Exception as e:
+        with open ("logs.txt", "a") as f:
+            f.write (f"error is occured {e}")
 
-        else:
-            print ("No need to UPDATE")
-
+# Execute Function
 add_tenant_to_cospace()
 
